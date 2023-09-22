462a9d5b5ff74c417541b62ba76e9956330af8dd
Neptune0403 <1109343209@qq.com>
2023-09-22 17:23:20 +0800

    Model info

--- model/Model.py
+++ model/Model.py
@@ -1,8 +1,11 @@
 from torch import nn
 
+from model.resnet import resnet50
+
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         # encoder (Resnet) 3 branches
-        
+        resnet = resnet50(pretrained=False)re
+        rene
